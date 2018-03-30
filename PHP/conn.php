2024e8dc768f6ffc0777fd5932cf9bb1ad59cb5d
jevuu <35980711+jevuu@users.noreply.github.com>
2018-03-29 20:21:12 -0400

    Update conn.php

--- PHP/conn.php
+++ PHP/conn.php
@@ -2,9 +2,9 @@
 
 //Handles connection to database
 
-$db_name = "panzooka_prj566";
-$mysql_username = "panzooka_prj566";
-$mysql_password = "5IGjANKHAxoUd6lF";
+$db_name = "panzooka_prj566";		//default = panzooka_prj566
+$mysql_username = "panzooka_prj566";	//default = panzooka_prj566
+$mysql_password = "";			//Please see Discord
 $server_name = "localhost";
 
 $conn = mysqli_connect($server_name, $mysql_username, $mysql_password, $db_name);
