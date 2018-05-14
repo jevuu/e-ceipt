f23d3800e1cdece541d777f1a426f6c2942fb568
Justin Vuu <35980711+jevuu@users.noreply.github.com>
2018-05-14 12:16:44 -0400

    Re-ordered table population

--- SQL/populateTables.sql
+++ SQL/populateTables.sql
@@ -17,8 +17,33 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8 */;
 
 --
--- Database: `panzooka_prj566`
+-- Database: `panzooka_prj666`
 --
+
+--
+-- Dumping data for table `test`
+--
+
+INSERT INTO `test` (`id`, `userName`, `name`, `created`, `comment`) VALUES
+(1, 'JohnDoe', 'John Doe', '2018-03-28 05:00:00', 'Hello World'),
+(2, 'JaneDoe', 'Jane Doe', '2018-03-29 00:40:27', 'Goodbye World');
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`userID`, `name`, `creationDate`, `photo`, `email`) VALUES
+(1, 'John Doe', '2018-04-09', NULL, NULL),
+(2, 'Jane Doe', '2018-04-09', '', ''),
+(5, 'Justin', '2018-04-09', NULL, NULL);
+
+--
+-- Dumping data for table `receipts`
+--
+
+INSERT INTO `receipts` (`receiptID`, `userID`, `creationDate`, `totalCost`, `tax`, `description`, `categoryID`) VALUES
+(1, 1, '2018-04-09', 0, 0, 'Trip to the college', NULL),
+(2, 2, '2018-04-09', 0, 0, 'Jane''s Receipt', NULL);
 
 --
 -- Dumping data for table `items`
@@ -37,31 +62,6 @@ INSERT INTO `receiptItems` (`receiptID`, `itemID`) VALUES
 (1, 1),
 (1, 2),
 (2, 3);
-
---
--- Dumping data for table `receipts`
---
-
-INSERT INTO `receipts` (`receiptID`, `userID`, `creationDate`, `totalCost`, `tax`, `description`, `categoryID`) VALUES
-(1, 1, '2018-04-09', 0, 0, 'Trip to the college', NULL),
-(2, 2, '2018-04-09', 0, 0, 'Jane''s Receipt', NULL);
-
---
--- Dumping data for table `test`
---
-
-INSERT INTO `test` (`id`, `userName`, `name`, `created`, `comment`) VALUES
-(1, 'JohnDoe', 'John Doe', '2018-03-28 05:00:00', 'Hello World'),
-(2, 'JaneDoe', 'Jane Doe', '2018-03-29 00:40:27', 'Goodbye World');
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`userID`, `name`, `creationDate`, `photo`, `email`) VALUES
-(1, 'John Doe', '2018-04-09', NULL, NULL),
-(2, 'Jane Doe', '2018-04-09', '', ''),
-(5, 'Justin', '2018-04-09', NULL, NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
