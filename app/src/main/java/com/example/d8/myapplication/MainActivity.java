0708d39a637ec2eec4d0d6cd1d1f54d417859ea2
Alistair Godwin <agodwin@myseneca.ca>
2018-05-16 14:25:24 -0400

    Hide Action Bar

--- app/src/main/java/com/example/d8/myapplication/MainActivity.java
+++ app/src/main/java/com/example/d8/myapplication/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.d8.myapplication;
 
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
+//Last modification: Alistair
+//Added a hide actionbar
 public class MainActivity extends AppCompatActivity {
     EditText userET, passET;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Hides the Actionbar
+        ActionBar ag = getSupportActionBar();
+        if(ag != null){
+            ag.hide();
+        }
 
         userET = (EditText)findViewById(R.id.main_uid);
         passET = (EditText)findViewById(R.id.main_pwd);
