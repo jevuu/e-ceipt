dbe4c8ddb68620b7ec260eca348cc5b50497be39
Alistair Godwin <agodwin@myseneca.ca>
2018-05-18 17:32:06 -0400

    firebase initial
    no pull request, no real changes

--- app/src/main/java/com/example/d8/myapplication/MainActivity.java
+++ app/src/main/java/com/example/d8/myapplication/MainActivity.java
@@ -6,11 +6,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 //Last modification: Alistair
 //Added a hide actionbar
 public class MainActivity extends AppCompatActivity {
+    //Attributes
     EditText userET, passET;
+   firebaseController firebase;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +27,28 @@ public class MainActivity extends AppCompatActivity {
 
         //Hides the Actionbar
         ActionBar ag = getSupportActionBar();
-        if(ag != null){
+        if(ag != null) {
             ag.hide();
         }
 
+
+
+
+
+
         userET = (EditText)findViewById(R.id.main_uid);
         passET = (EditText)findViewById(R.id.main_pwd);
+    }
+
+
+    //The initial function after onCreate()
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in
+         firebase.currentUser = mAuth.getCurrentUser();
+        Toast.makeText(this, "User Already Logged in? Invalid Exit.",
+                Toast.LENGTH_SHORT).show();
     }
 
     public void onRegister(View view){
@@ -34,6 +59,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
     public void onLogin(View view) {
+
+
+
         String userString = userET.getText().toString();
         String passString = ""; //passET.getText().toString();
         String type = "login";
--- app/src/main/java/com/example/d8/myapplication/User.java
+++ app/src/main/java/com/example/d8/myapplication/User.java
@@ -5,6 +5,30 @@ package com.example.d8.myapplication;
  */
 
 class User {
+    //====Atr====//
+  private  String userName;
+  private String passWord;
+    //==============//
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+
+
+
 
 
 }
