20ceae13e610aade328f6eb701be86f187f9f7a7
jevuu <35980711+jevuu@users.noreply.github.com>
2018-05-23 16:36:25 -0400

    added business info

--- PHP/getUserReceipts.php
+++ PHP/getUserReceipts.php
@@ -16,12 +16,19 @@ if($userName == ""){
 $receipts = array();
 
 //Get all receipts from userName
-$qString = "SELECT u.name, r.receiptID, r.creationDate, r.totalCost, r.tax 
+$qString = "SELECT u.name, r.receiptID, r.creationDate, r.totalCost, r.tax, r.businessID 
 FROM users u 
 INNER JOIN receipts r ON r.userID = u.userID 
 WHERE u.userName= '$userName'" ;
 $qReceipt= mysqli_query($conn, $qString);
 while($receipt= mysqli_fetch_row($qReceipt)){
+	
+	$qString = "SELECT u.name
+	FROM users u
+	INNER JOIN businesses b ON b.userID = u.userID
+	WHERE b.businessID = $receipt[5] limit 1";
+	$qBusiness = mysqli_query($conn, $qString);
+	$businessName = mysqli_fetch_object($qBusiness);
 	
 	//Build an array of items from the receipt
 	//Then push into the "items" array
@@ -43,12 +50,13 @@ while($receipt= mysqli_fetch_row($qReceipt)){
 	
 	//Build the receipt array and push into "receipts" array
 	$tempReceipt = [
-		'name' 		=> $receipt[0],
-		'receiptID' => $receipt[1],
-		'date' 		=> $receipt[2],
-		'totalCost' => $receipt[3],
-		'tax' 		=> $receipt[4],
-		'items'		=> $items
+		'name' 			=> $receipt[0],
+		'receiptID' 	=> $receipt[1],
+		'date' 			=> $receipt[2],
+		'totalCost' 	=> $receipt[3],
+		'tax' 			=> $receipt[4],
+		'items'			=> $items,
+		'businessName'	=> $businessName->name
 	];
 	array_push($receipts, $tempReceipt);
 }
