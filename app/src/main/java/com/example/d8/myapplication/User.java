b343a59dfc1b862e6e4b1fbe39740a54f26db5a5
KignorC <kignorchan@gmail.com>
2018-06-07 20:07:13 -0400

    Remove unuseful code in User class

--- app/src/main/java/com/example/d8/myapplication/User.java
+++ app/src/main/java/com/example/d8/myapplication/User.java
@@ -6,13 +6,7 @@ class User {
     //====Atr====//
     String nickName;
     String userId;
-
-    String firebaseUID;
     String email;
-    String name;
-    String creationDate;
-    String photoURL;
-    boolean isLogin;
 
     public String getEmail() {
         return email;
@@ -37,45 +31,5 @@ class User {
     public void setUserId(String userId) {
         this.userId = userId;
     }
-
-
-    public String getFirebaseUID() {
-        return firebaseUID;
-    }
-
-    public void setFirebaseUID(String firebaseUID) {
-        this.firebaseUID = firebaseUID;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(String creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public String getPhotoURL() {
-        return photoURL;
-    }
-
-    public void setPhotoURL(String photoURL) {
-        this.photoURL = photoURL;
-    }
-
-    public boolean isLogin() {
-        return isLogin;
-    }
-
-    public void setLogin(boolean login) {
-        isLogin = login;
-    }
+    
 }
