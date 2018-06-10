50225cb056fd19385af791a50e74979bd5cc1454
Justin Vuu <35980711+jevuu@users.noreply.github.com>
2018-06-10 12:44:08 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/d8/myapplication/MainActivity.java
+++ app/src/main/java/com/example/d8/myapplication/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.d8.myapplication;
 
 import android.content.Intent;
-import android.support.annotation.MainThread;
 import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,6 +13,7 @@ import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
@@ -26,7 +27,11 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
 
-//Last modification: Alistair
+
+import static android.content.ContentValues.TAG;
+
+
+//Last modification: Alistair 6/7/2018
 //Firebase Support Added
 
 public class MainActivity extends AppCompatActivity {
@@ -41,6 +46,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         //Firebase
         aUser = new authUser();
 
@@ -54,6 +60,16 @@ public class MainActivity extends AppCompatActivity {
         passET = (EditText)findViewById(R.id.main_pwd);
         btnSign = (Button)findViewById(R.id.main_btn_login);
         btnGms = (Button)findViewById(R.id.sign_in_google);
+
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+
+        aUser.mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+
+
     }
 
 
@@ -62,32 +78,23 @@ public class MainActivity extends AppCompatActivity {
     public void onStart() {
         super.onStart();
         // Check if user is signed in
-        if(aUser.isLoggedIn()) {
-            Toast.makeText(this, "User Already Logged in? Invalid Exit.",
-                    Toast.LENGTH_SHORT).show();
-         FirebaseAuth.getInstance().signOut();
-        }
+        FirebaseAuth.getInstance().signOut();
+        aUser.mGoogleSignInClient.signOut();
+
     }
 
     @Override
     public void onDestroy(){
-    super.onDestroy();
+        super.onDestroy();
         FirebaseAuth.getInstance().signOut();
-
+        aUser.mGoogleSignInClient.signOut();
 
     }
 
-    //Opens the Registration Activity
+    //Opens the Google Sign In applicaiton
     public void onGoogle(View view){
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        aUser.mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
         Intent signInIntent = aUser.mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, 9001);
-
-
 
     }
     @Override
@@ -121,6 +128,8 @@ public class MainActivity extends AppCompatActivity {
                             Toast.makeText(MainActivity.this, "Google Auth Passed",
                                     Toast.LENGTH_SHORT).show();
 
+                            onReady(this, "a");
+
                         } else {
                             // If sign in fails, display a message to the user.
                             Toast.makeText(MainActivity.this, "Google Auth Failed",
@@ -133,17 +142,21 @@ public class MainActivity extends AppCompatActivity {
 
     //Opens the Registration Activity
     public void onRegister(View view){
-    Intent goToReg = new Intent(this, RegisterActivity.class);
-    startActivity(goToReg);
-
-    }
-    //Opens the Registration Activity
-    public void onReady(OnCompleteListener<AuthResult> view){
-        Intent goToReg = new Intent(this, MenuActivity.class);
-        //Intent goToReg = new Intent(this, HomeActivity.class);
+        Intent goToReg = new Intent(this, RegisterActivity.class);
         startActivity(goToReg);
 
-   }
+    }
+    //Opens the Home Activity
+    public void onReady(OnCompleteListener<AuthResult> view, String execType){
+        //Excute VM connections
+        aUser.contactSql_log(this);
+        Information.authUser = aUser;
+
+
+        Intent goToReg = new Intent(this, HomeActivity.class);
+        startActivity(goToReg);
+
+    }
     //Opens the Password Reset  Activity
     public void onPassW(View view){
         Intent goToPws = new Intent(this, PassActivity.class);
@@ -181,34 +194,56 @@ public class MainActivity extends AppCompatActivity {
             aUser.mAuth.signInWithEmailAndPassword(userString, passString).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                 @Override
                 public void onComplete(@NonNull Task<AuthResult> task) {
-                    if (task.isSuccessful()) {
+                    aUser.MUser();
+                    aUser.createUser();
+
+                    if (task.isSuccessful() && aUser.mUser.isEmailVerified()) {
                         // Sign in success, update UI with the signed-in user's information
-                        Toast.makeText(MainActivity.this, "Authentication Passed",
+                        btnSign.setText(getString(R.string.main_login));
+                        btnSign.setClickable(true);
+                        Toast.makeText(MainActivity.this, "Welcome to ECeipt",
+                                Toast.LENGTH_SHORT).show();
+
+                        onReady(this, type);
+
+                    } else if(task.isSuccessful() && aUser.mUser.isEmailVerified() == false) {
+                        //============Fail States for Sign In=================
+
+                        Toast.makeText(MainActivity.this, "Please verify your email to continue, an email has been resent",
                                 Toast.LENGTH_SHORT).show();
                         btnSign.setText(getString(R.string.main_login));
                         btnSign.setClickable(true);
-                        //Go to home screen
-                        onReady(this);
+                        //Send Validation Email
+                        aUser.mUser.sendEmailVerification()
+                                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()) {
+                                            Log.d(TAG, "Email sent!");
+                                        }else{
 
+                                            Log.d(TAG, "Failed Email Verification!");
+                                        }
+                                    }
+                                });
 
-                    } else {
-                        // If sign in fails, display a message to the user.
-                        Toast.makeText(MainActivity.this, "Authentication Failed.",
+                    }else {
+                        Toast.makeText(MainActivity.this, "Username/Password Incorrect",
                                 Toast.LENGTH_SHORT).show();
                         btnSign.setText(getString(R.string.main_login));
                         btnSign.setClickable(true);
-
                     }
                 }
+                //===================================//
+
             });
-            //My.SQL VM Connection
-            BackgroundWorker bgWorker = new BackgroundWorker(this);
-            bgWorker.execute(type, userString, passString);
+
 
 
         }
         //===================================//
 
     }
+
 
 }
