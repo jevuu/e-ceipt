630e53ac8af2c941753f1a15e27aca726fb2409c
Justin Vuu <35980711+jevuu@users.noreply.github.com>
2018-06-10 12:30:39 -0400

    Update BackgroundWorker.java

--- app/src/main/java/com/example/d8/myapplication/BackgroundWorker.java
+++ app/src/main/java/com/example/d8/myapplication/BackgroundWorker.java
@@ -34,8 +34,8 @@ public class BackgroundWorker extends AsyncTask<String, String, String> {
     @Override
     protected String doInBackground(String... params) {
         String type = params[0];
-        String login_URL = "http://myvmlab.senecacollege.ca:6207/login.php";
-        if(type.equals("login")){
+        String login_URL = params[1];
+        if(type.equals("register")){
 
             OutputStream os = null;
             InputStream is = null;
@@ -45,19 +45,10 @@ public class BackgroundWorker extends AsyncTask<String, String, String> {
                     //constants
                     URL url = new URL(login_URL);
                     JSONObject jsonObject = new JSONObject();
-                    jsonObject.put("userName", params[1]);
-                    /*
-                    jsonObject.put("comment", "OK");
-                    jsonObject.put("category", "pro");
-                    jsonObject.put("day", "19");
-                    jsonObject.put("month", "8");
-                    jsonObject.put("year", "2015");
-                    jsonObject.put("hour", "16");
-                    jsonObject.put("minute", "41");
-                    jsonObject.put("day_of_week", "3");
-                    jsonObject.put("week", "34");
-                    jsonObject.put("rate_number", "1");
-                    */
+                    jsonObject.put("userName", params[2]);
+                    jsonObject.put("name", params[3]);
+                    jsonObject.put("email", params[4]);
+
                     String message = jsonObject.toString();
 
                     conn = (HttpURLConnection) url.openConnection();
@@ -81,33 +72,6 @@ public class BackgroundWorker extends AsyncTask<String, String, String> {
                 //clean up
                 os.flush();
 
-                /*
-                String userName = params[1];
-                //String password = params[2];    //There is no password property in the SQL table. This data isn't passed.
-
-
-                URL url = new URL(login_URL);
-                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
-                httpURLConnection.setRequestMethod("POST");
-                httpURLConnection.setDoOutput(true);
-                httpURLConnection.setRequestProperty("Content-Type","application/x-www-form-urlencoded");
-                httpURLConnection.setDoInput(true);
-                //httpURLConnection.setFixedLengthStreamingMode(queryString.getBytes().length);
-                //Uri.Builder builder = new Uri.Builder().appendQueryParameter("userName", userName);
-                //String query = builder.build().getEncodedQuery();
-
-                OutputStream os = httpURLConnection.getOutputStream();
-                BufferedWriter bWriter = new BufferedWriter(new OutputStreamWriter(os, "UTF-8"));
-                String postData = URLEncoder.encode("userName", "UTF-8")+"="+URLEncoder.encode(userName,"UTF-8");
-                                //+"&"+URLEncoder.encode("password", "UTF-8")+"="+URLEncoder.encode(password,"UTF-8");
-                bWriter.write(postData);
-                bWriter.flush();
-                bWriter.close();
-                os.close();
-
-                //httpURLConnection.connect();
-                */
-                //InputStream
                 is = conn.getInputStream();
                 BufferedReader bReader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"));
                 String result = "";
@@ -127,6 +91,63 @@ public class BackgroundWorker extends AsyncTask<String, String, String> {
                 e.printStackTrace();
             }
         }
+        else if(type.equals("login")){
+
+            OutputStream os = null;
+            InputStream is = null;
+            HttpURLConnection conn = null;
+
+            try {
+                //constants
+                URL url = new URL(login_URL);
+                JSONObject jsonObject = new JSONObject();
+                jsonObject.put("userName", params[2]);
+
+
+                String message = jsonObject.toString();
+
+                conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout( 10000 /*milliseconds*/ );
+                conn.setConnectTimeout( 15000 /* milliseconds */ );
+                conn.setRequestMethod("POST");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+                conn.setFixedLengthStreamingMode(message.getBytes().length);
+
+                //make some HTTP header nicety
+                conn.setRequestProperty("Content-Type", "application/json;charset=utf-8");
+                conn.setRequestProperty("X-Requested-With", "XMLHttpRequest");
+                //open
+                conn.connect();
+                //setup send
+                os = new BufferedOutputStream(conn.getOutputStream());
+                os.write(message.getBytes());
+                //clean up
+                os.flush();
+
+                is = conn.getInputStream();
+                BufferedReader bReader = new BufferedReader(new InputStreamReader(is, "iso-8859-1"));
+                String result = "";
+                String line = "";
+                while((line = bReader.readLine()) != null){
+                    result += line;
+                }
+                bReader.close();
+                is.close();
+                conn.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+
+
+
+        }
         return null;
     }
 
@@ -140,7 +161,7 @@ public class BackgroundWorker extends AsyncTask<String, String, String> {
     @Override
     protected void onPostExecute(String result){
         aDialog.setMessage(result);
-        aDialog.show();
+        System.out.println(result);
 
     }
 
