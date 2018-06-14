113b440dcae9a3dd2f14ce0a1c5e4112fcdd35ae
Alistair Godwin <agodwin@myseneca.ca>
2018-06-14 13:38:41 -0400

    Take Photo Functionality
    Needs to utilize the image

--- app/src/main/java/com/example/d8/myapplication/OCRTextActivity.java
+++ app/src/main/java/com/example/d8/myapplication/OCRTextActivity.java
@@ -1,5 +1,8 @@
 package com.example.d8.myapplication;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 //Initial Class for Textual Activity Reading
@@ -12,4 +15,30 @@ public class OCRTextActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ocrtext);
     }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    //==================== Get User Photo/Camera =======================//
+
+//This function executes a valid bitmap intent from the camera.
+    private void takePhoto() {
+        Intent take = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (take.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(take, REQUEST_IMAGE_CAPTURE);
+        }
+
+    }
+
+    //Retuns the photo apps bitmap for use.
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+        }
+    }
+
+    //=========================================================//
+
+
 }
