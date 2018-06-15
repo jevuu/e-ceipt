ae317452320f8546ffaeeea06a0e3f67480e4bb0
Alistair Godwin <agodwin@myseneca.ca>
2018-06-14 22:33:22 -0400

    Requests Permissions for Android 7+
    Camera Request Feature is required on most recent APIs. Additionally checks to ensure a camera app exists.

--- app/src/main/java/com/example/d8/myapplication/OCRTextActivity.java
+++ app/src/main/java/com/example/d8/myapplication/OCRTextActivity.java
@@ -1,19 +1,34 @@
 package com.example.d8.myapplication;
 
+import android.*;
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.provider.MediaStore;
+import android.support.annotation.RequiresApi;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
 //Initial Class for Textual Activity Reading
 //This class handles the primary bulk of text OCR
 //Last Modifcation: 6/11/2018
 public class OCRTextActivity extends AppCompatActivity {
-
+    TextView txt_add;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ocrtext);
+        txt_add = (TextView) findViewById(R.id.edtErr);
+        takePhoto();
     }
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
@@ -21,12 +36,53 @@ public class OCRTextActivity extends AppCompatActivity {
     //==================== Get User Photo/Camera =======================//
 
 //This function executes a valid bitmap intent from the camera.
+
     private void takePhoto() {
-        Intent take = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (take.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(take, REQUEST_IMAGE_CAPTURE);
+        try {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                    != PackageManager.PERMISSION_GRANTED){
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    requestPermissions(new String[]{Manifest.permission.CAMERA},
+                            100);
+                }
+
+            }else {
+
+                Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                }
+            }
+
+        }catch(Exception e){
+            Toast.makeText(this, "No Default App Available! " + e.getMessage(),
+                    Toast.LENGTH_SHORT).show();
+            txt_add.setText(e.getMessage());
+
+
         }
 
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        if(grantResults.length >= 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            takePhoto();
+        }else{
+            Toast.makeText(this, "You Cannot take a photo without the camera!",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+
+    }
+
+    public static boolean isIntentAvailable(Context context, String action) {
+        final PackageManager packageManager = context.getPackageManager();
+        final Intent intent = new Intent(action);
+        List<ResolveInfo> list =
+                packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
     }
 
     //Retuns the photo apps bitmap for use.
