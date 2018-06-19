abbe99288c75579a724c7e80e3fc74bf04fa568b
KignorC <kignorchan@gmail.com>
2018-06-18 23:19:50 -0400

    Add syncronize data feature back!

--- app/src/main/java/com/example/d8/myapplication/MainActivity.java
+++ app/src/main/java/com/example/d8/myapplication/MainActivity.java
@@ -152,7 +152,7 @@ public class MainActivity extends AppCompatActivity {
         //Excute VM connections
         aUser.contactSql_log(this);
         Information.authUser = aUser;
-        //DataController.SyncronizeData("http://myvmlab.senecacollege.ca:6207/getUserReceipts.php", this);
+        DataController.SyncronizeData("http://myvmlab.senecacollege.ca:6207/getUserReceipts.php", this);
 
         Intent goToReg = new Intent(this, MenuActivity.class);
 
