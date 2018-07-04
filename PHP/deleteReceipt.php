925e61f51c28034379f9a153de26bc5152eba135
jevuu <35980711+jevuu@users.noreply.github.com>
2018-07-04 15:13:18 -0400

    successfully tested deleteReceipt

--- PHP/deleteReceipt.php
+++ PHP/deleteReceipt.php
@@ -10,12 +10,12 @@ $qString = "DELETE FROM receipts WHERE receiptID = " . $rID;
 mysqli_query($conn, $qString);
 
 //Test deletion
-$qString = "SELECT FROM receipts WHERE receiptID = " . $rID;
+/*$qString = "SELECT FROM receipts WHERE receiptID = " . $rID;
 $result = mysqli_query($conn, $qString);
 if(mysqli_num_rows($result) == 0){
 	echo "Receipt was deleted! <br />";
 }else{ 
 	echo "Receipt was not deleted! <br />";
-}
+}*/
 
 ?>
